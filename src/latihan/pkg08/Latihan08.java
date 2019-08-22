e5fd84da25a196ebe4fd9401dd9142afbc3dbfc1
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2019-08-22 14:46:04 +0700

    first commit

---
+++ src/latihan/pkg08/Latihan08.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg08;
+
+/**
+ *
+ * @author yusri
+ */
+public class Latihan08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x= 12;
+        int y= 11;
+        
+        y= ++x;
+        System.out.println("Nilai y sekarang:"+y);
+        System.out.println("hasil x sekarang:"+x);
+        
+        
+    } 
+}
